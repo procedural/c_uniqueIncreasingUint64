058a2396407e41dab9136ce99b9935c7b95f4d20
procedural <iamvfx@gmail.com>
2021-05-15 03:36:27 +0300

    Create unique_increasing.h

---
+++ unique_increasing.h
@@ -0,0 +1,13 @@
+#pragma once
+
+uint64_t _uniqueIncreasingUint64 (const char * key, uint64_t value);
+
+/*
+return _unique("foo", 0); // OK
+...
+return _unique("bar", 0); // OK
+...
+return _unique("foo", 0); // Compile time error: _uniqueIncreasingUint64("foo", 0) was used already, use _uniqueIncreasingUint64("foo", 1) now
+...
+return _unique("bar", 32); // Compile time error: _uniqueIncreasingUint64("bar", 1) hasn't been used yet, use it first
+*/
