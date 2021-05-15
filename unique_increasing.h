4e893c3b4b773836637f5b412531bdb9ec1e9c1d
procedural <iamvfx@gmail.com>
2021-05-15 03:48:22 +0300

    Update unique_increasing.h

--- unique_increasing.h
+++ unique_increasing.h
@@ -1,6 +1,6 @@
 #pragma once
 
-uint64_t _uniqueIncreasingUint64 (const char * key, uint64_t value);
+#define _uniqueIncreasingUint64(key, value) value
 
 /*
 return _uniqueIncreasingUint64("foo", 0); // OK
