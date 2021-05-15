6cbc1c2d128a081c8b9fca173634e744afc8c7f8
procedural <iamvfx@gmail.com>
2021-05-15 03:39:12 +0300

    Update unique_increasing.h

--- unique_increasing.h
+++ unique_increasing.h
@@ -7,7 +7,7 @@ return _unique("foo", 0); // OK
 ...
 return _unique("bar", 0); // OK
 ...
-return _unique("foo", 0); // Compile time error: _uniqueIncreasingUint64("foo", 0) is used already, use _uniqueIncreasingUint64("foo", 1) now
+return _unique("foo", 0); // Compile time error: "_uniqueIncreasingUint64("foo", 0) is used already, use _uniqueIncreasingUint64("foo", 1) now."
 ...
-return _unique("bar", 32); // Compile time error: _uniqueIncreasingUint64("bar", 1) hasn't been used yet, use it first
+return _unique("bar", 32); // Compile time error: "_uniqueIncreasingUint64("bar", 1) hasn't been used yet, use it first."
 */
